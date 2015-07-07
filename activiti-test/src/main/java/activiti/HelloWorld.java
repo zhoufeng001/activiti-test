0dd42130b7cc67e9538655384dce7be790829c2a
is_zhoufeng <is_zhoufeng@163.com>
2015-07-08 02:33:23 +0800

    update

--- activiti-test/src/main/java/activiti/HelloWorld.java
+++ activiti-test/src/main/java/activiti/HelloWorld.java
@@ -1,13 +1,20 @@
 package activiti;
 
+import java.util.List;
+
 import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.ProcessEngines;
 import org.activiti.engine.RepositoryService;
+import org.activiti.engine.TaskService;
 import org.activiti.engine.repository.Deployment;
 import org.activiti.engine.repository.DeploymentBuilder;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
 import org.junit.Test;
 
 public class HelloWorld {
+    
+    ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
     
     /**
      * 部署流程定义
@@ -25,6 +32,44 @@ public class HelloWorld {
 	System.out.println(deployment.getName());
 	System.out.println(deployment.getDeploymentTime());
 	System.out.println(deployment.getCategory()); 
+    }
+    
+    /**
+     * 启动引擎实例
+     */
+    @Test
+    public void startProcessInstance(){
+	ProcessInstance processInstance = processEngine.getRuntimeService().startProcessInstanceByKey("helloworld"); 
+	System.out.println(processInstance.getId());
+	System.out.println(processInstance.getActivityId());
+	System.out.println(processInstance.getBusinessKey());
+	System.out.println(processInstance.getProcessDefinitionId());
+	System.out.println(processInstance.getProcessInstanceId());
+	System.out.println(processInstance.getProcessVariables()); 
+    }
+    
+    /**
+     * 获取当前人的任务信息
+     */
+    @Test
+    public void findMyTask(){
+	TaskService taskService = processEngine.getTaskService();
+	List<Task> tasks = taskService.createTaskQuery().taskAssignee("李四").list(); 
+	if(tasks != null){
+	    for (Task task : tasks) {
+		System.out.println(task.getId() + "  " + task.getName() + "  " + task.getProcessDefinitionId()); 
+	    }
+	}
+    }
+    
+    /**
+     * 完成我的任务
+     */
+    @Test
+    public void doTask(){
+	TaskService taskService = processEngine.getTaskService();
+	taskService.complete("402"); 
+	System.out.println("完成任务402");
     }
 
 }
